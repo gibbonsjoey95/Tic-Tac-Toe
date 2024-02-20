04f35b38b08b1e33345a4bd0a9e7c44f7174eb9d
Joey <gibbonsjoey95@gmail.com>
2024-02-19 21:49:30 -0600

    player can now make a choice

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
 const gameBoard = (function () {
-    let gameBoard = new Array(9)
+    let board = ['', '', '', '', '', '', '', '', '']
 
-    const logBoard = () => {
-        console.log(gameBoard)
-    }
-
-    return logBoard
+    return { board }
 })();
 
-gameBoard()
+const player = (function () {
+    let player1 = 'x'
+    let player2 = 'o'
+
+    return {player1, player2}
+})()
+
+const game = (function () {
+    const startingPlayer = player.player1
+    let playerTurn = player.player1
+
+    const playerChoice = (choice) => {
+        let index = gameBoard.board[choice - 1]
+
+        if(index === ''){
+            gameBoard.board[choice - 1] = playerTurn
+        }
+
+        if(playerTurn === startingPlayer){
+            playerTurn = player.player2
+        } else {
+            playerTurn = player.player1
+        }
+
+        return gameBoard.board
+    }
+
+    return {playerChoice}
+})()
+
